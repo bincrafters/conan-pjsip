1863acbc0f63b46cc41dd8e67762c5c3a190290d
bincrafters-user <bincrafters@gmail.com>
2020-09-30 07:07:44 +0000

    Update Conan conventions
    Automatically created by bincrafters-conventions 0.26.1

--- conanfile.py
+++ conanfile.py
@@ -23,7 +23,7 @@ class PJSIPConan(ConanFile):
     _source_subfolder = "source_subfolder"
     _build_subfolder = "build_subfolder"
 
-    requires = "openssl/1.1.1g"
+    requires = "openssl/1.1.1h"
 
     def config_options(self):
         if self.settings.os == 'Windows':
